b0cdfe7a74bec716d0c03b7e1a57cb14edf4ba75
Nabendu <nabendu.biswas@gmail.com>
2021-07-06 06:37:00 +0530

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
 import './App.css';
 
+const getImages = async query => {
+  const url = "https://unsplash-worker.theweb-dev.workers.dev"
+
+  const resp = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify({ query }),
+    headers: { 'Content-type': 'application/json'}
+  })
+  return resp.json()
+}
+
 function App() {
+  const [query, setQuery] = useState("")
+  const [images, setImages] = useState([])
+
+  useEffect(() => {
+    const initialSearch = async () => {
+      const results = await getImages('Coding')
+      setImages(results)
+    }
+    initialSearch()
+  },[])
+
+  const search = async event => {
+    event.preventDefault();
+    const results = await getImages(query)
+    setImages(results)
+  }
+
+  const updateQuery = event => setQuery(event.target.value)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form className="flexContainer" onSubmit={search}>
+        <label htmlFor="query"><h2>Image Search: </h2></label>
+        <input id="query" type="text" className="inputStyle" value={query} onChange={updateQuery} placeholder="Search Image" />
+        <button type="submit">Search</button>
+      </form>
+      <div className="imgContainer">
+        {images.map(image => <a key={image.id} href={image.link} target="_blank">
+          <img src={image.image} />
+        </a>)}
+      </div>
+    </>
   );
 }
 
